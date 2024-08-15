e96e3c3beb925b76392040808571f3846fb95097
a-gafron <gafron-alex@gmx.de>
2024-08-15 21:19:12 +0200

    update the getInput() now with the right order and values of features

--- frontend/main_front_end.py
+++ frontend/main_front_end.py
@@ -38,16 +38,31 @@ class Depressiveness(QDialog):
         
     def getInputs(self):
         inputs = []
-        inputs.append(float(self.ui.comboBox_0_1.currentText()))  # age
-        inputs.append(0 if self.ui.comboBox_1_1.currentText() == "female" else 1)  # gender
+        
+        # age
+        inputs.append(float(self.ui.comboBox_0_1.currentText()))
+        
+        # gender
+        inputs.append(0 if self.ui.comboBox_1_1.currentText() == "female" else 1)
+        
+        # bmi inlcuding the calculation 
         height = float(self.ui.comboBox_2_1.currentText())
         weight = float(self.ui.comboBox_3_1.currentText())
         bmi = weight / ((height / 100) ** 2)
-        inputs.append(bmi)  # bmi berechnet
-        inputs.append(float(self.ui.comboBox_2_3.currentText()))  # gad-score
-        inputs.append(1 if self.ui.comboBox_0_3.currentText() == "yes" else 0)  # anxiety_diagnosis
-        inputs.append(float(self.ui.comboBox_4_1.currentText()))  # epworth_score
-        inputs.append(1 if self.ui.comboBox_3_3.currentText() == "yes" else 0)  # depressiveness_diagnosis
+        inputs.append(bmi)
+        
+        # epworth_score
+        inputs.append(float(self.ui.comboBox_4_1.currentText()))  
+                
+        # gad_score
+        inputs.append(float(self.ui.comboBox_2_3.currentText()))
+        
+        # depressiveness_awareness
+        inputs.append(1 if self.ui.comboBox_3_3.currentText() == "yes" or self.ui.comboBox_4_3.currentText() == "yes"else 0)
+        
+        # anxiety_awareness
+        inputs.append(1 if self.ui.comboBox_0_3.currentText() == "yes" or self.ui.comboBox_1_3.currentText() == "yes" else 0)  
+                
         return inputs
     
     def predict(self):
